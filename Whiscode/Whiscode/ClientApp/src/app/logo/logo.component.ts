91dab1ea4133e4e9e2764f23c10d2301cc240b77
Whiscode <sarah.giblette@gmail.com>
2020-04-26 22:33:52 +0100

    Added SVG logo component. Incomplete - needs styling and animation

--- Whiscode/Whiscode/ClientApp/src/app/logo/logo.component.ts
+++ Whiscode/Whiscode/ClientApp/src/app/logo/logo.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-logo',
-  templateUrl: './logo.component.html',
+  templateUrl: './logo.component.svg',
   styleUrls: ['./logo.component.css']
 })
 export class LogoComponent implements OnInit {
