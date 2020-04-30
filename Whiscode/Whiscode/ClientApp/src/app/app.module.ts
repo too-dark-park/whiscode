38b3aef2f0a664376fc8b17cb6bc4781465266a6
Whiscode <sarah.giblette@gmail.com>
2020-04-30 15:06:40 +0100

    Added support for icons by installing Angular Material

--- Whiscode/Whiscode/ClientApp/src/app/app.module.ts
+++ Whiscode/Whiscode/ClientApp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon'; // TODO create a module file dedicated to material module
 
 import { AppComponent } from './app.component';
 import { LogoComponent } from './logo/logo.component';
@@ -20,7 +21,8 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
