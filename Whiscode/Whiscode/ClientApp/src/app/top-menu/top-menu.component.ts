3b79283b8d085f3bfcc2e8c8f3ec135014c367f7
Whiscode <sarah.giblette@gmail.com>
2020-05-02 17:51:04 +0100

    Added CSS animation and styling for the navigation

--- Whiscode/Whiscode/ClientApp/src/app/top-menu/top-menu.component.ts
+++ Whiscode/Whiscode/ClientApp/src/app/top-menu/top-menu.component.ts
@@ -9,6 +9,16 @@ export class TopMenuComponent implements OnInit {
 
   constructor() { }
 
+  displayNavigation: boolean = false;
+  transition: string = ''; // Stops CSS animations playing on page load
+
   ngOnInit(): void {
+  }
+
+  toggleNavigation() {
+    this.displayNavigation = !this.displayNavigation;
+    if (this.transition == '') {
+      this.transition = 'hide';
+    }
   }
 }
