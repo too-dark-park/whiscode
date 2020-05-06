e0a7e489ef6e5b070eab12df614af7f35338031a
Whiscode <sarah.giblette@gmail.com>
2020-05-06 12:20:09 +0100

    Added a form to the contact page. Contains validation and styling but does not send a message - just a console output.

--- Whiscode/Whiscode/ClientApp/src/app/contact-form/contact-form.component.ts
+++ Whiscode/Whiscode/ClientApp/src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent implements OnInit {
 
-  constructor() { }
+  contactForm;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.contactForm = this.formBuilder.group({
+      name: this.nameFormControl,
+      email: this.emailFormControl,
+      message: this.messageFormControl
+    });
+  }
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  nameFormControl = new FormControl('', [
+    Validators.required
+  ]);
+
+  messageFormControl = new FormControl('', [
+    Validators.required
+  ]);
 
   ngOnInit(): void {
   }
 
+  onSubmit(customerData) {
+    this.contactForm.reset();
+
+    console.log(this.contactForm.valid)
+
+    console.warn('Your order has been submitted', customerData);
+  }
 }
